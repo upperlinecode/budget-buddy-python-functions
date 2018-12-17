d2b7e730e3f23213fdbb9d69651ea7dabe475368
Aaron <afink.2020@saintannsny.org>
2018-12-16 20:56:58 -0500

    New, prewritten tests for the first three methods.

--- budgetTest.py
+++ budgetTest.py
@@ -1,9 +1,31 @@
 import budget
 
-# Call your methods here with test values to see if they work the way you expect them to.
+# In this file you will be testing the different budget checker functions you wrote in the budget.py file.
 # Remember that since your methods will be RETURNING information, it will only be visible if you PRINT that return value out to the console.
 # Also, because the methods are in a different file, you must access them using dot syntax like "budget.simple_budget_checker()" to let the
 #     computer know where to find their definitions.
-# Your first test might look like this:
 
-print(budget.simple_budget_checker(120))
+# Here are tests for the first three functions you wrote. Uncomment them to see them in action.
+# Notice that they test both pass and fail cases, to make sure the functions work for any input.
+
+#print(budget.simple_budget_checker(75)) # This test should print nothing because the 75 is less than 100 dollars.
+#print(budget.simple_budget_checker(120)) # This test should print a warning because 120 is more than 100 dollars.
+
+#print(budget.budget_checker(132)) # This test should print a warning because the budget has been exceeded.
+#print(budget.budget_checker(69)) # This test should print a message saying that the user is under budget.
+
+#print(budget.precise_budget_checker(100)) # This test should alert the user that they are at their maximum budget.
+#print(budget.precise_budget_checker(132)) # This test should print a warning because the budget has been exceeded.
+#print(budget.precise_budget_checker(69)) # This test should print a message saying that the user is under budget.
+
+
+
+
+# Here you should try writing your own tests for ultimate_budget_checker().
+# Make sure that you test each possible outcome so you know the function is working for any input, not just a success case.
+
+
+
+
+# Once again, write your own tests for flexible_budget_checker().
+# Try testing it with different budgets and different totals to make sure it always returns the correct result.
